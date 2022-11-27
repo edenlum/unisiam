97f68a6c4ff57c3e61df01398d2a9bdc256317b9
edenlum <edenlum@gmail.com>
2022-11-27 22:58:26 +0200

    Update r10.sh

--- config/unisiam/mini/r10.sh
+++ config/unisiam/mini/r10.sh
@@ -2,5 +2,5 @@ python train.py \
   --dataset miniImageNet \
   --backbone resnet10 \
   --lrd_step \
-  --data_path [your mini-imagenet-folder] \
-  --save_path [your save-folder]
+  --data_path /content/unisiam/train \
+  --save_path /content/unisiam/out
